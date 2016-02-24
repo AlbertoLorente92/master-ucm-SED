328adab78e239b7b3c1f64cd9765f4e64376e632
Hristo Ivanov <hristo.idgh@gmail.com>
2016-02-24 15:50:52 +0100

    Organización del repositorio.

--- p1/ES_Programada/Debug/led.d
+++
@@ -1,9 +0,0 @@
-led.d led.o: ../led.c C:\hlocal\EclipseARMwspace\p1\commonEclipse/44b.h \
- C:\hlocal\EclipseARMwspace\p1\commonEclipse/option.h \
- C:\hlocal\EclipseARMwspace\p1\commonEclipse/44blib.h
-
-C:\hlocal\EclipseARMwspace\p1\commonEclipse/44b.h:
-
-C:\hlocal\EclipseARMwspace\p1\commonEclipse/option.h:
-
-C:\hlocal\EclipseARMwspace\p1\commonEclipse/44blib.h:
