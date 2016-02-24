a516bc91a2c22b65774b2380693c796768e246fa
Hristo Ivanov <hristo.idgh@gmail.com>
2016-02-24 16:03:03 +0100

    P1. ES_Interupciones. Crear.

---
+++ p1/ES_Interrupciones/main.c
@@ -0,0 +1,31 @@
+/*--- ficheros de cabecera ---*/
+#include "44blib.h"
+#include "44b.h"
+#include "stdio.h"
+/*--- funciones externas ---*/
+extern void leds_off();
+extern void led1_on();
+extern void leds_switch();
+extern void Eint4567_init(void);
+//extern void sys_init();
+/*--- declaracion de funciones ---*/
+//void Main(void);
+/*--- codigo de funciones ---*/
+
+void Main(void)
+{
+	/* Inicializar controladores */
+	 rPCONG &= ~(1<<6 & 1<<7);
+	 rPUPG = 0;
+	//sys_init(); // Inicializacion de la placa, interrupciones y puertos
+	/* Establecer valor inicial de los LEDs */
+	leds_off();
+	led1_on();
+	while (1){
+		if ((rPDATG & (1<<6)) == 0 || (rPDATG & (1<<7)) == 0 ){
+			leds_switch();
+			//DelayMs(100);
+			while((rPDATG & (1<<6)) == 0 || (rPDATG & (1<<7)) == 0){}
+		}
+	}
+}
